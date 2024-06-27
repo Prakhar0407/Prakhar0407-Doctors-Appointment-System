a23d7cedfae0fe6cfe60a4fd36a5ebc60df54312
Prakhar <prakharsankle@gmail.com>
2024-06-27 19:16:21 +0530

    Merging with MongoDB and Cloudinary

--- Backend/App.js
+++ Backend/App.js
@@ -1,15 +1,32 @@
 import express from "express";
 
-import {config} from "dotenv"
-import cors from "cors"
+import {config} from "dotenv";
+import cors from "cors"; //connecting frontend
 const app= express();
 config({ path: "./config/config.env" });
+import fileUpload from "express-fileupload";
+
+import { databaseworking } from "./database/databaseworking.js";
+import cookieParser from "cookie-parser";
+
+app.use(cookieParser());
+app.use(express.json());
+
+databaseworking();
 
 app.use(
     cors({
-      origin: [process.env.FRONTEND_URL, process.env.FRONTEND_URL],
-      method: ["GET", "POST", "DELETE", "PUT"],
-      credentials: true,
+        origin: [process.env.FRONTEND_URL, process.env.DASHBOARD_URL],
+        method: ["GET", "POST", "DELETE", "PUT"],
+        credentials: true,
+    })
+);
+app.use(express.urlencoded({extended: true}));  //date etc. readable
+
+app.use(  //accoring to documentaion
+    fileUpload({
+      useTempFiles: true,
+      tempFileDir: "/tmp/",
     })
   );
 
