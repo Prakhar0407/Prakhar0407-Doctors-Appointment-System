8e5665c9c7d5d49f394e1c7d31db70ebdc60bdfe
Prakhar <prakharsankle@gmail.com>
2024-06-27 18:00:27 +0530

    Connecting to frontend

--- Backend/App.js
+++ Backend/App.js
@@ -1,8 +1,16 @@
 import express from "express";
 
-
-
+import {config} from "dotenv"
+import cors from "cors"
 const app= express();
+config({ path: "./config/config.env" });
 
+app.use(
+    cors({
+      origin: [process.env.FRONTEND_URL, process.env.FRONTEND_URL],
+      method: ["GET", "POST", "DELETE", "PUT"],
+      credentials: true,
+    })
+  );
 
-export default AbortControllerpp;
+export default app;
