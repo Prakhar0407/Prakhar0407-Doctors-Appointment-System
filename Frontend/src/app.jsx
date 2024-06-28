3e26f610f40256dec3a07cc3b607188312d3c2d5
Prakhar <prakharsankle@gmail.com>
2024-06-28 18:58:36 +0530

    Modify and Create Folders for Frontend

--- Frontend/src/app.jsx
+++ Frontend/src/app.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
 import "./app.css"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 const app = () => {
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/appointment" element={<Appointment />} />
+          <Route path="/about" element={<AboutUs />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+        <ToastContainer position="top-center" />
+      </Router>
+    </>
+  );
 }
 
 export default app
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,4 +2,32 @@ import { render } from 'preact'
 import { App } from './app.jsx'
 import ReactDom from 'react-dom/client'
 
-render(<App />, document.getElementById('app'))
+export const Context = createContext({
+    isAuthenticated: false,
+  });
+  
+  const AppWrapper = () => {
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const [user, setUser] = useState({});
+  
+    return (
+      <Context.Provider
+        value={{
+          isAuthenticated,
+          setIsAuthenticated,
+          user,
+          setUser,
+        }}
+      >
+        <App />
+      </Context.Provider>
+    );
+  };
+  
+
+rReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+      <AppWrapper />
+    </React.StrictMode>
+  );
+  
