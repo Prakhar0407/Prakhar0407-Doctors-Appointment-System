0fc4c2349e84845b8d1c81c7e9159a7b6e51ca59
Prakhar <prakharsankle@gmail.com>
2024-06-28 15:57:08 +0530

    Modify Get messages

--- Backend/controller/messageControl.js
+++ Backend/controller/messageControl.js
@@ -17,5 +17,13 @@ export const sendMessage = async (req, res, next) => {
   });
 };
 
+export const getAllMessages = catchAsyncErrors(async (req, res, next) => {
+  const messages = await Message.find();
+  res.status(200).json({
+    success: true,
+    messages,
+  });
+});
+
 
 
