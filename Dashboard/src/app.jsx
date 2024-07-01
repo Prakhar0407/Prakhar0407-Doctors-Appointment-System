038a4c4afb790987dd5639c8761a18838b4b5442
Prakhar <prakharsankle@gmail.com>
2024-07-01 12:45:55 +0530

    Introducing Dashboard

---
+++ Dashboard/src/app.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./app.css";
+
+
+const app = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default app
