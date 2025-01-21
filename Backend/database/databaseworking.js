3b648371674ad62ab12bcdf10927358f3248750c
Prakhar <prakharsankle@gmail.com>
2025-01-21 13:43:28 +0530

    Update database

--- Backend/database/databaseworking.js
+++ Backend/database/databaseworking.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const databaseworking = () => {
   
     mongoose.connect(process.env.MONGO_URI, {
-      dbName: "DOCTORS-APPOINTMENT-APP",  
+      dbName: "Appointment_System_Deploy",  
       
     })
     .then(() => {
