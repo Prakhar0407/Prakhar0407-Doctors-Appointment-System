d5dc43e2c7783239eebb85d5c8f143c21bce58c4
Prakhar <97653096+Prakhar0407@users.noreply.github.com>
2025-06-14 18:52:37 +0530

    Update Register.jsx

--- Frontend/src/pages/Register.jsx
+++ Frontend/src/pages/Register.jsx
@@ -3,9 +3,10 @@ import React, { useContext, useState } from "react";
 import { toast } from "react-toastify";
 import { Context } from "../main";
 import { Link, Navigate, useNavigate } from "react-router-dom";
-const VITE_BASE_URL = import.meta.env.VITE_BASE_URL || "http://localhost:4000";
+
 const Register = () => {
   const { isAuthenticated, setIsAuthenticated } = useContext(Context);
+
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [email, setEmail] = useState("");
@@ -21,11 +22,7 @@ const Register = () => {
     try {
       await axios
         .post(
-<<<<<<< HEAD
-          `http://localhost:4000/api/v1/user/patient/register`,
-=======
-          `https://docapp-server-atoj.onrender.com/api/v1/user/patient/register`,
->>>>>>> 6025e4bbb85f728086851b9467e902699a712691
+          "http://localhost:4000/api/v1/user/patient/register",
           { firstName, lastName, email, phone, dob, gender, password },
           {
             withCredentials: true,
